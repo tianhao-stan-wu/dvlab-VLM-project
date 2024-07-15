8d8a591ce7d8ac672a626b788a73301127fea564
tianhao-stan-wu <wutianha@usc.edu>
2024-07-15 22:19:50 +0800

    add debug prints

--- test.py
+++ test.py
@@ -2,15 +2,23 @@ from PIL import Image
 import requests
 from transformers import AutoProcessor, LlavaForConditionalGeneration
 
+print('starting test program...')
+print('loading llava model...')
 model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
+
+print('loading processor...')
 processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
 
+print('loading image...')
 prompt = "USER: <image>\nWhat's the content of the image? ASSISTANT:"
 url = "https://www.ilankelman.org/stopsigns/australia.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
+print('processor generating inputs...')
 inputs = processor(text=prompt, images=image, return_tensors="pt")
 
 # Generate
+print('model generating outputs...')
 generate_ids = model.generate(**inputs, max_new_tokens=15)
+print('processor decoding outputs...')
 processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
